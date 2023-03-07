68ac2da90f297a812dc946f99f00cc8cad3bf998
Gary M <gary.fullstackdev@gmail.com>
2023-03-07 12:50:33 -0500

    added aboutme

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 Ubuntu and Git usage, Assignment one for FSDI-111
 
-<<<<<<< HEAD
 Added a few more lines
-=======
-Added a few more lines
->>>>>>> b8f4b68 (about me added to assignment)
--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
 @app.route('/aboutme', methods=['GET'])
 def about_me():
     heres_me={'firstname': 'Gary', 'lastname': 'M.', 'hobbies': 'pc-gaming' }
