c23ae11fc5ebaf57c1c79b6d84d164fb4cc06d0a
Gary M <gary.fullstackdev@gmail.com>
2023-03-06 20:14:28 -0500

    files for assignment 1

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+"""stuffhere"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/aboutme")
+def hello():
+    return "<h1> Hello Folks</h1>"
